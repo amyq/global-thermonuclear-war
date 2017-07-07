a48e9b6209476ea9b6c70c4060c63d9608bec2a2
Amy Qualls <amyq@acquia.com>
2017-07-07 13:41:19 -0700

    testing out global include

--- source/conf.py
+++ source/conf.py
@@ -97,6 +97,7 @@ html_theme = 'bootstrap'
 # html_theme_options = {}
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 html_sidebars = {'**': ['localtoc.html', 'globaltoc.html', 'searchbox.html', 'sourcelink.html']}
+rst_prolog = open("_includes/acquia-products.txt", "r").read()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
