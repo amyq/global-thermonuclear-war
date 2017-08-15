68f1063cf5aa1b2512bc5c17de7adb36ecc88f90
Amy Qualls <amyq@acquia.com>
2017-08-15 10:37:53 -0700

    changing case of Pillow to pillow

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinxcontrib.fulltoc',
-    'Pillow']
+    'pillow']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
